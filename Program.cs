027a0485a7f4fcc40f36e6687f5b820ce868ef28
SHUSKEY1 <108241894+SHUSKEY1@users.noreply.github.com>
2022-10-16 17:44:13 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            int[] RanArray = new int[1000];
+            int[] counter = new int[10];
+            
+            Random random = new Random();
+            for(int i=0; i < 1000; i++)
+            {
+                RanArray[i] = random.Next(0, 9);
+                
+            }
+
+           foreach(int value in RanArray)
+            {
+                Console.WriteLine(value);
+            }
+
+            counter[RanArray[i]++];
+
+                int k = 0;
+
+              for (int i = 0; i < 10; i++)
+                {
+
+
+                    for (int j = 0; j < counter[i]; j++)
+                    {
+                        RanArray[k++] = i;
+                    }
+                }
+            
+
+           
+
+        }
+
+        
+    }
+}
